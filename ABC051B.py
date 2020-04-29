a41556ee72ca1a84e192e1a29ff166ac5488dd22
elun <64539748+elun-ph@users.noreply.github.com>
2020-04-30 01:56:59 +0900

    commit test

---
+++ ABC051B.py
@@ -0,0 +1,11 @@
+import sys
+f = open("input.txt", "r")
+sys.stdin = f
+
+k, s = map(int, input().split())
+cnt = 0
+for x in range(k+1):
+    for y in range(k+1):
+        if 0 <= s-x-y <= k:
+            cnt += 1
+print(cnt)
