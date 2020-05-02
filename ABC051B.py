e7813698b173136fe20ba66eae880369adffc913
elun <64539748+elun-ph@users.noreply.github.com>
2020-05-02 10:05:16 +0900

    AC

--- ABC051B.py
+++ ABC051B.py
@@ -4,8 +4,10 @@ sys.stdin = f
 
 k, s = map(int, input().split())
 cnt = 0
+
 for x in range(k+1):
     for y in range(k+1):
-        if 0 <= s-x-y <= k:
+        if 0 <= s - x - y <= k:
             cnt += 1
+
 print(cnt)
